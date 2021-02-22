dd0076eb138bd4236ee32115f2c2644f5cc57ad1
ted523 <tedotieno523@gmail.com>
2021-02-22 10:35:25 +0300

    Added Authentication and Tokens

---
+++ spotify/util.py
@@ -0,0 +1,14 @@
+from .models import SpotifyToken
+from django.utils import timezone
+from datetime import timedelta
+
+def get_user_tokens(session_id):
+    user_tokens = SpotifyToken.objects.filter(user=session_id)
+    if user_tokens.exists():
+        return user_tokens[0]
+    else:
+        return None
+
+def update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token):
+    tokens = get_user_tokens(session_id)
+    expires_in = timezone.now() + timedelta(seconds=expires_in)
